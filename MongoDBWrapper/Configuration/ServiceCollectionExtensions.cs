5e6faaca5ba6132a17c647430620d1b9b66d4f48
mdauy <marion.daussy@gmail.com>
2022-06-04 13:32:08 +0200

    MongoDb access configuration

---
+++ MongoDBWrapper/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MongoDBWrapper.Configuration
+{
+    /// <summary>
+    /// ServiceCollectionExtensions to declare DI that's proper to the wrapper.
+    /// TODO : add it to the Startup class (API, Console, etc.).
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Injects the configuration service.
+        /// </summary>
+        /// <param name="services">ServiceCollection.</param>
+        public static void AddMongodbWrapperConfiguration(this IServiceCollection services)
+        {
+            services.AddScoped<MongoDbWrapperConfiguration>();
+        }
+    }
+}
---
+++ MongoDBWrapper/Repositories/IBaseRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+
+namespace MongoDBWrapper.Repositories
+{
+    /// <summary>
+    /// Generic BaseRepository to use to access any collection.
+    /// </summary>
+    /// <typeparam name="T">Any Document type.</typeparam>
+    public interface IBaseRepository<T>
+    {
+        /// <summary>
+        /// Get T object from id.
+        /// </summary>
+        /// <param name="id">ObjectId</param>
+        /// <returns>Searched Document.</returns>
+        Task<T> Get(string id);
+
+        /// <summary>
+        /// Insert complete DAO to targeted collection.
+        /// </summary>
+        /// <param name="document">DAO to insert.</param>
+        /// <returns>Inserted DAO.</returns>
+        Task<T> Insert(T document);
+
+        /// <summary>
+        /// Delete specidif document from database.
+        /// </summary>
+        /// <param name="document">Document to delete.</param>
+        /// <returns>Deletion success.</returns>
+        Task<bool> Delete(T document);
+
+        /// <summary>
+        /// Update complete document.
+        /// </summary>
+        /// <param name="document">Document to update.</param>
+        /// <returns>Update success.</returns>
+        Task<bool> Update(T document);
+
+        /// <summary>
+        /// Get documents corresponding to a statement.
+        /// </summary>
+        /// <param name="expression">Statement that must be verified to retrieve documents.</param>
+        /// <returns>Corresponding documents.</returns>
+        Task<IList<T>> Get(Expression<Func<T, bool>> expression);
+    }
+}
